1e7c3374c89a3da93dddaa88fb1ef35b33ac8d16
Marwanishere <marwan.hasan.work@gmail.com>
2023-08-05 06:50:13 +0100

    testing methods of generating urls

--- extension.py
+++ extension.py
@@ -1,7 +1,8 @@
+#below is made using the help of cs50.ai
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/index.html')
 def home():
-    message = "Hello, CS50!"
+    message = "Hello, CS50!!"
     return render_template('index.html', message=message)
